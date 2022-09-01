ec64d5eb8c1ea289bfc009bfd8e663f4c9c1bbb4
alexbournemsk <arbiter1@yandex.ru>
2022-09-01 11:16:13 +0300

    css update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [todo, setTodo] = useState('')
+  const [todos, setTodos] = useState([])
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault()
+
+    const newTodo = {
+      id: Math.floor(Math.random() * 10000),
+      text: todo,
+      completed: false
+    }
+
+    setTodos([newTodo, ...todos])
+    setTodo("")
+  }
+
+  const deleteTodo = (id) => {
+    let updatedTodos = [...todos].filter((todo) => todo.id !== id)
+    setTodos(updatedTodos)
+  }
+
+  const toggleComplete = (id) => {
+    let updatedTodos = [...todos].map((todo) => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    })
+
+    setTodos(updatedTodos)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={todo}
+          onChange={(evt) => { setTodo(evt.target.value) }}
+        />
+
+        <button type="submit" >Добавить задачу</button>
+      </form>
+      {todos.map((todo) =>
+        <div className = 'todo-list' key={todo.id}>
+          <div
+            className = {(todo.completed) ? 'todo-row-complete' : 'todo-row'}
+            onClick = {() => toggleComplete(todo.id)}
+          >
+
+            <input type="checkbox" checked={todo.completed} />
+            {todo.text}
+
+          </div>
+          <button onClick={() => deleteTodo(todo.id)}>x</button>
+        </div>
+
+
+      )}
     </div>
   );
 }
